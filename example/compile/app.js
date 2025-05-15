00a0068e723da2c6a37ed13dcb5d7d75c1444267
Mariano <amarianochavez@gmail.com>
2025-05-15 17:58:28 -0300

    fix: update i18n language from Spanish to English in example

--- example/compile/app.js
+++ example/compile/app.js
@@ -44,7 +44,7 @@ if (!window.React) {
       return React.createElement("div", {
         className: "App"
       }, React.createElement(RichTextEditor, {
-        i18n: 'es',
+        i18n: 'en',
         readOnly: false,
         modules: {
           table: {},
@@ -98,4 +98,3 @@ if (!window.React) {
     reportError('Error in application initialization: ' + e.message);
   }
 }
-//# sourceMappingURL=app.js.map
